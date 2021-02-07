acf7695db59973ad5f228c7fe2ea7ea8bc91b4c5
Beives <38698358+Beives@users.noreply.github.com>
2021-02-07 11:45:56 +0100

    created reusable admin components

---
+++ admin/includes/nav.php
@@ -0,0 +1,85 @@
+
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <a class="navbar-brand" href="#">Navbar</a>
+
+            <!-- Top Menu Items -->
+            <ul class="nav justify-content-end">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Messages
+                      </a>
+                    <div class="dropdown-menu"  aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">
+                                <div class="media">
+                                    <img class="align-self-start mr-3" src="http://placehold.it/50x50" alt="">
+                                    <div class="media-body">
+                                        <h5>
+                                            <strong>John Smith</strong>
+                                        </h5>
+                                        <p class="small text-muted">Yesterday at 4:32 PM</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
+                                    </div>
+                                </div>
+                            </a>
+                        <div><hr class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Read All New Messages</a>
+                    </div>
+                </li>
+
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Notification
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="#" class="dropdown-item">Alert Name <span class="label label-default">Alert Badge</span></a>
+
+                        <a href="#" class="dropdown-item">Alert Name <span class="label label-primary">Alert Badge</span></a>
+
+                        <a href="#" class="dropdown-item">Alert Name <span class="label label-success">Alert Badge</span></a>
+
+                        <a href="#" class="dropdown-item">Alert Name <span class="label label-info">Alert Badge</span></a>
+
+                        <a href="#" class="dropdown-item">Alert Name <span class="label label-warning">Alert Badge</span></a>
+
+                        <a href="#" class="dropdown-item">Alert Name <span class="label label-danger">Alert Badge</span></a>
+
+                        <div class="dropdown-divider"></div>
+
+                        <a href="#" class="dropdown-item">View All</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Profile
+                    </a>
+                    <div class="dropdown-menu">
+                        
+                        <a href="#"  class="dropdown-item">Profile</a>
+                        
+                        <a href="#"  class="dropdown-item">Inbox</a>
+
+                        <a href="#"  class="dropdown-item">Settings</a>
+                        
+                        <div class="dropdown-divider"></div>
+                        
+                        <a href="#"  class="dropdown-item"> Log Out</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="row">
+
+            <div class="col-lg-1">
+                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                  <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+                  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+                  <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+                  <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                </div>
+            </div>
